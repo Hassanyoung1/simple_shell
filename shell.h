7c2df367255bac9e06600bbfcde628afc6374566
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-05-16 12:51:18 -0700

    newshell

---
+++ shell.h
@@ -0,0 +1,19 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <stdbool.h>                                                            
+#include <stdio.h>                                                              
+#include <stdlib.h>                                                             
+#include <string.h>                                                             
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/wait.h>                                                                     
+
+/**helper function*/
+void free_vector(char **argv);
+int _strlen(char *str);
+char **token_split(char *path, char *delim);
+void prompt_exect(char **argv, char **envp);
+
+
+#endif
+
