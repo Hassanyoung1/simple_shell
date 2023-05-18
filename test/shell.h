964efa63e62d923025f2039046e19e495420ebe6
Abubakar Adam Sani <Athernee@gmail.com>
2023-05-18 06:40:48 +0000

    uploaded test directory

---
+++ test/shell.h
@@ -0,0 +1,25 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+
+/* Helper functions */
+void free_vector(char **argv);
+int _strlen(char *str);
+
+
+char **token_split(char *path, char *delim);
+void prompt_exect(char **argv, char **envp);
+
+
+
+
+#endif
