49e6e937d0b7e4843ed589eebcae9abeb51963b3
Abubakar Adam Sani <Athernee@gmail.com>
2023-05-23 06:47:48 +0000

    "merged beta files. refined code"

--- shell.h
+++ shell.h
@@ -1,16 +1,32 @@
 #ifndef SHELL_H
 #define SHELL_H
-#include <stdbool.h>                                                            
-#include <stdio.h>                                                              
-#include <stdlib.h>                                                             
-#include <string.h>                                                             
-#include <unistd.h>
-#include <sys/types.h> 
-#include <sys/wait.h>                                                                     
 
-/**helper function*/
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/**HELPER FUNCTIONS*/
 void free_vector(char **argv);
 int _strlen(char *str);
+char *call_getline(char *str, size_t n);
+int stringcmp(char *s1, char *s2);
+/*void fork_check(int pid);*/
+void fork_check(char **argv, char **envp, pid_t pid);
+char *_strdup(char *str);
+char *_strcat(char *dest, char *src);
+char *path_get(char *command);
+char *path_get_new(char *command);
+bool check_builtin(char *str);
+
+/* EXTERNS*/
+extern char **environ;
+
+/*MAIN FUNCTIONS*/
 char **token_split(char *path, char *delim);
 void prompt_exect(char **argv, char **envp);
 
--- z_test_files/shell.h
+++ z_test_files/shell.h
@@ -4,22 +4,21 @@
 
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <stdlib.h>
 
-
-/* Helper functions */
+/**helper function*/
 void free_vector(char **argv);
 int _strlen(char *str);
+char *call_getline(char *str, size_t n);
 
 
 char **token_split(char *path, char *delim);
 void prompt_exect(char **argv, char **envp);
 
 
-
-
 #endif
+
